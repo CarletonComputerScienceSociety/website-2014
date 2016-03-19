898dba2fd12e34af7627f45b2c9ebedeecbd13cc
Sam Whiteley <sam.whiteley@hotmail.com>
2016-03-19 16:18:31 -0400

    Check for existant x added (plus testable x)

--- server/components/vote/index.js
+++ server/components/vote/index.js
@@ -4,12 +4,23 @@
 
 'use strict';
 
+//BEGIN testVars
+var testx = require('./testx.js').x;
+//END testVars
+
 module.exports.ballot = function (req, res) {
   var viewFilePath = 'ballot';
 
   var x = req.query.x; //token passed from SCS to us
 
+  //BEGIN test
+  if(typeof x === 'undefined' || x === undefined || x === '') {
+    x = testx;
+  }
+  //END test
+
   //TODO
+  // (done) ensure the send an x
   // decode x (from base64 byte string) to utf8
   // decrypt x (Blowfish)
   // assign x uid unique to x
@@ -17,5 +28,14 @@ module.exports.ballot = function (req, res) {
   // if not, allow x to vote
   // if so, or if invalid x, or if invalid other params, send error.
 
+  //check to make sure they sent an x (redirect from the SCS login)
+  if(typeof x === 'undefined' || x === undefined || x === '') {
+    res.status(401).send('Try logging in, cheater.');
+    return;
+  } else {
+    console.log(x);
+  }
+
   res.render(viewFilePath);
+
 };
